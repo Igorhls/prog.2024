99bc979602cd6c1f35ea72a0d205a04871e9ca64
Igorhls <igorhls@hotmail.com>
2024-07-22 19:26:02 -0300

    Adicionado arquivos da aula do dia 22/07

---
+++ Igor/UDP-FileServer/00.udp.v0.umbloco/udp-file-client/udp-file-client.py
@@ -0,0 +1,24 @@
+import socket
+
+SERVER = '127.0.0.1'
+PORT   = 12345
+sock = socket.socket (socket.AF_INET, socket.SOCK_DGRAM)
+
+while True:
+    # Lê do usuário o nome do arquivo a pedir ao servidor
+    fileName = input("Arquivo a pedir ao servidor: ")
+
+    # Envia ao servidor do nome do arquivo desejado pelo usuário
+    print ("Enviando pedido a", (SERVER, PORT), "para", fileName)
+    sock.sendto (fileName.encode('utf-8'), (SERVER, PORT))
+
+    # Recebe o conteúdo do arquivo vindo do servidor
+    data, source = sock.recvfrom (4096)
+
+    # Grava o arquivo localmente
+    print ("\nGravando arquivo localmente")
+    fd = open (fileName, 'wb')
+    fd.write (data)
+    fd.close()
+
+sock.close()
